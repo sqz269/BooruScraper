ec91073ebd99d90eec048fea60cb51677819fbe1
sqz269 <yin2690855381@gmail.com>
2020-05-22 10:42:23 -0500

    fixed classname and inheritance

--- Scraper/components/gelbooru/components.py
+++ Scraper/components/gelbooru/components.py
@@ -3,7 +3,7 @@ from Scraper.framework.components_basic import ComponentBasic
 from bs4 import BeautifulSoup
 import requests
 
-class ComponentYandere(BaseComponent):
+class ComponentGelbooru(ComponentBasic):
 
     def __init__(self):
         super().__init__()
