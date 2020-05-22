2fdd0598c87ad19a16f98ec1130fc95e9b47735e
sqz269 <yin2690855381@gmail.com>
2020-05-21 23:45:51 -0500

    added initial files for gelbooru scraper

---
+++ Scraper/components/gelbooru/components.py
@@ -0,0 +1,9 @@
+from Scraper.framework.components_basic import ComponentBasic
+
+from bs4 import BeautifulSoup
+import requests
+
+class ComponentYandere(BaseComponent):
+
+    def __init__(self):
+        super().__init__()
